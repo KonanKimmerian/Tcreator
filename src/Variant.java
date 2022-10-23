95040d7336c60196d828a23b824dbdc5896c4d1b
KonanKimmerian <112086367+KonanKimmerian@users.noreply.github.com>
2022-10-23 12:18:21 +0300

    Add files via upload

---
+++ src/Variant.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Variant {
+
+
+    ArrayList<Question> questions;
+
+    public Variant(ArrayList<Question>questions) {
+        this.questions = questions;
+    }
+
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+}
+
